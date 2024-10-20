7456f46c7a65e7bdd3883cd303135695b77a6448
gusalviarez <gusalv2001@gmail.com>
2024-10-20 13:51:49 -0400

    last save

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,9 @@
 if status is-interactive
     starship init fish | source
+    zoxide init fish | source
 end
 
 set -U fish_greeting
+
+alias es='setxkbmap -layout es'
+alias us='setxkbmap -layout us'
